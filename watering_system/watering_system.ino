e82378b2176f57ff3d3316f6e65efcc61b0b6374
tridooo <torrydo.dev@gmail.com>
2023-12-16 15:12:50 +0700

    refactor

--- watering_system/watering_system.ino
+++ watering_system/watering_system.ino
@@ -2,38 +2,46 @@
 #define BLYNK_TEMPLATE_NAME "plant watering"
 #define BLYNK_AUTH_TOKEN "7ipFMtKuX0ghOOHdU0N_rBTY9vNzwq7o"
 
+char ssid[] = "Android Xr";
+char password[] = "1234567899";
+
+
 #include <DHT.h>
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 
 
-char ssid[] = "Android Xr";
-char password[] = "1234567899";
 
-
+// ----------------------------- the pin ports of the esp8266 module ---------------------------------------------
 const int pin_dht = D1;
 const int pin_relay = D2;
 const int pin_button = D5;
 const int pin_led = D8;
 
+
+// -----------------------------the virtual ports of the blynk service --------------------------------------------
 const int blynk_vpin_pump = V2;
 const int blynk_vpin_led = V8;
 
 
+// ------------------------------------------- sensors ------------------------------------------------------------
 const int dht_type = DHT11;
 DHT dht(pin_dht, dht_type);
 
-int threshold_humidity = 40;
 
+// -------------------------------------------- states ------------------------------------------------------------
+int threshold_humidity = 40;
 int soil_moisture = -1;
 int humidity = -1;
 int temperature = -1;
 
-boolean isButtonActivated = 1;  // check
-boolean isAuto = 1;   // check
+boolean isButtonActivated = 1;
+boolean isAuto = 1;
 
 unsigned long lastTime = millis();
 int count = 0;
+
+
 
 void setup() {
 
@@ -63,6 +71,12 @@ void setup() {
   
 }
 
+/*
+* toggle main led status
+*
+* @param state: true or false
+* @param setState: default true, push the state to the blynk or not
+*/
 void setMainLed(int state, bool setState = true){
   if(state == HIGH){
     digitalWrite(pin_led, HIGH);
@@ -71,6 +85,12 @@ void setMainLed(int state, bool setState = true){
   }
 }
 
+/*
+* toggle pump status
+*
+* @param state: true or false
+* @param setState: default true, push the state to the blynk or not
+*/
 void setPump(int state, bool setState = true){
   if(state == HIGH){
     digitalWrite(pin_relay, HIGH);
@@ -87,6 +107,10 @@ int isPumpRunning(){
   return digitalRead(pin_relay);
 }
 
+/*
+* listen humidity and temperature via sensor
+* then write to the global variables
+*/
 void handleHdt(){
     float h = dht.readHumidity();
     float t = dht.readTemperature();
@@ -106,6 +130,10 @@ void handleHdt(){
     Serial.print("Do am: " + String(h));
 }
 
+/*
+* listen soil moisture value via the sensor
+* then write to the global variable
+*/
 void handleSoilMoisture(){
     int tempe = analogRead(A0);
     tempe = map(tempe, 0, 1023, 100, 0);
@@ -117,6 +145,10 @@ void handleSoilMoisture(){
     Serial.println(" <> Độ ẩm đất: " + String(soil_moisture));
 }
 
+/*
+* listen button click event 
+* if the button is clicked, reverse pump state 
+*/
 void handleClickButton(){
   if(digitalRead(pin_button) == LOW){
     isButtonActivated = !isButtonActivated;
@@ -155,15 +187,23 @@ void loop() {
   
 }
 
+// ------------------------------------- listen data from blynk ---------------------------------------------------
+
+/*
+* set humidity threshold, 
+* eg: if the humidity threshold = 50, then the pump will be turned on if the environment humidity smaller than 50%
+*/
 BLYNK_WRITE(V30){
   int value = param.asInt();
 
   Serial.print("updateThresholdHumidity: " + String(value));
 
   threshold_humidity = value;
-
 }
 
+/*
+* set auto pump state
+*/
 BLYNK_WRITE(V31){
   int value = param.asInt();
   if(value == 0){
@@ -173,6 +213,10 @@ BLYNK_WRITE(V31){
   }
 }
 
+/*
+* turn on or off the button
+* turn off auto pump if this function is called
+*/
 BLYNK_WRITE(V2){
   int value = param.asInt();
   Blynk.virtualWrite(V31, 0);
@@ -182,5 +226,4 @@ BLYNK_WRITE(V2){
   }else{
     setPump(HIGH);
   }
-  
 }
